80ada866f9beedbe8fbed75c89d35abbb03457e2
Garth <jmort47@hotmail.com>
2014-11-10 08:50:11 -0700

    First attempt at setting up WordPress after unpacking.

--- lib/wp-brokenness-exercises_02.php
+++ lib/wp-brokenness-exercises_02.php
@@ -5,6 +5,11 @@ function setup_kits() {
 	if( !isset( $_POST['exerciseChoice'] ) ) { die; }
 
 	$kit = $_POST['exerciseChoice'];
+	preg_match( '/\d+/', $kit, $kitnum );
+
+	$dbname = $_POST['db_name'];
+	$dbuser = $_POST['db_user'];
+	$dbpass = $_POST['db_pass'];
 
 	$zip = new ZipArchive;
 	$res = $zip->open("../wpzip/$kit");
@@ -12,13 +17,18 @@ function setup_kits() {
 		$zip->extractTo("../");
 		$zip->close();
 		echo "Unzipped $kit";
+
+		chdir("../TestPress$kitnum['0']");
+		`wpcli db reset --yes`;
+		echo "$dbname has been cleared.";
+		`wpcli core config --dbname=$dbname --dbuser=$dbuser --dbpass=$dbpass`;
+		echo "$dbname is has been setup for Exercise $kitnum['0'].";
 	}
 	else {
 		echo "Failed to unzip $kit";
 	}
 
-	header("Location: " . $_SERVER['HTTP_REFERER']);
-}	
+}
 
 setup_kits();
 ?>
