b9ae4529946b3bab9cdaade1b92c8892a1e0bed5
Garth <jmort47@hotmail.com>
2014-11-11 15:50:19 -0700

    Fix my stupid jQuery error in validating.

--- media/main.js
+++ media/main.js
@@ -1,19 +1,19 @@
 $(document).ready(function(e) {
 
 	$('input[type="submit"]').click(function( e ) {
-		if( $('input[name="db_name"]').value == null || $('input[name="db_name"]').value == "" ) {
+		if( $('input[name="db_name"]').val() == null || $('input[name="db_name"]').val() == "" ) {
 			e.preventDefault();
 			alert('Please enter your database name.');
 			return false;
 		}
 
-		if( $('input[name="db_user"]').value == null || $('input[name="db_user"]').value == "" ) {
+		if( $('input[name="db_user"]').val() == null || $('input[name="db_user"]').val() == "" ) {
 			e.preventDefault();
 			alert('Please enter your database username.');
 			return false;
 		}
 
-		if( $('input[name="db_pass"]').value == null || $('input[name="db_pass"]').value == "" ) {
+		if( $('input[name="db_pass"]').val() == null || $('input[name="db_pass"]').val() == "" ) {
 			e.preventDefault();
 			alert('Please enter your database password.');
 			return false;
