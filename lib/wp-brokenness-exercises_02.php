fd493551c7083d4465f3b7b81c8d3de0dee53177
Garth <jmort47@hotmail.com>
2014-11-12 09:09:05 -0700

    I think the installer is working now, all my tests worked.

--- lib/wp-brokenness-exercises_02.php
+++ lib/wp-brokenness-exercises_02.php
@@ -7,15 +7,11 @@ function setup_kits() {
 	$kit = $_POST['exerciseChoice'];
 	preg_match( '/[0-9]/', $kit, $num );
 
-	$kitnum = $kit[9];
+	$kitnum = $num[0];
 
 	$dbname = escapeshellarg( $_POST['db_name'] );
 	$dbuser = escapeshellarg( $_POST['db_user'] );
 	$dbpass = escapeshellarg( $_POST['db_pass'] );
-
-	print $dbname;
-	print $dbuser;
-	print $dbpass;
 
 	$zip = new ZipArchive;
 	$res = $zip->open("../wpzip/$kit");
@@ -29,11 +25,35 @@ function setup_kits() {
 		system( 'rm -rf wp-config.php' );
 //		system( "wpcli db reset --yes" );
 //		echo "$dbname has been cleared.";
-		passthru( "wp core config --dbuser=$dbuser --dbpass=$dbpass --dbname=$dbname --dbprefix='wp_kjyh_'" );
-		exec( "wp core config --dbuser=$dbuser --dbpass=$dbpass --dbname=$dbname --dbprefix='wp_kjyh_'" );
-		system( "wp core config --dbuser=$dbuser --dbpass=$dbpass --dbname=$dbname --dbprefix='wp_kjyh_'" );
-		shell_exec( "wp core config --dbuser=$dbuser --dbpass=$dbpass --dbname=$dbname --dbprefix='wp_kjyh_'" );
-		echo "$dbname has been setup for Exercise $kitnum.";
+//		system( "wp core config --dbuser=$dbuser --dbpass=$dbpass --dbname=$dbname --dbprefix='wp_kjyh_'" );
+		`cp wp-config-sample.php wp-config.php`;
+		`sed -i "s/'database_name_here'/$dbname/" wp-config.php`;
+		`sed -i "s/'username_here'/$dbuser/" wp-config.php`;
+		`sed -i "s/'password_here'/$dbpass/" wp-config.php`;
+		`sed -i "s/'wp_'/'wp_kjyh_'/" wp-config.php`;
+		$clear = 'wpcli db reset --yes';
+		exec($command,$output=array(),$worked);
+
+		switch ($worked) {
+
+			case 0:
+				echo "$dbname has been cleared.";
+				break;
+			case 1:
+				echo "There was an error clearing $dbname, your exercise may not be setup correctly. Please try again.";
+				break;
+		}
+		$command = 'mysql -u' . $dbuser . ' -p' . $dbpass . ' ' . $dbname .' < database.sql';
+		exec($command,$output=array(),$worked);
+		switch ($worked) {
+
+			case 0:
+				echo "$dbname has been setup for Exercise $kitnum.";
+				break;
+			case 1:
+				echo "There was an error importing $dbname.";
+				break;
+		}
 	}
 	else {
 		echo "Failed to unzip $kit";
